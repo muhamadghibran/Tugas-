ea2f7d5248dcc65b54a9235568927796457e7030
muhamadghibran <ripkaka91@gmail.com>
2024-02-20 06:18:57 +0700

    Update

--- LuasSegitiga.py
+++ LuasSegitiga.py
@@ -1,11 +1,11 @@
-luas = int(input("Masukan Luas: "))
-keliling = int(input("Masukan Keliling: "))
+alas = int(input("Masukan Luas: "))
+Tinggi = int(input("Masukan Keliling: "))
 
 sisi1 = int(input(" Masukan Sisi 1: "))
 sisi2 = int(input(" Masukan Sisi 2: "))
 sisi3 = int(input(" Masukan Sisi 3: "))
 
-luasSegitiga = 1/2 * luas * keliling
+luasSegitiga = 1 / 2 * alas * Tinggi
 kelilingSegitiga = sisi1 + sisi2 + sisi3
 
 print("Luas Segitiga:  %.0f"%(luasSegitiga))
