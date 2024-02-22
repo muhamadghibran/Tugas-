737ff34babe8a3f5c66522ad5ab6228d41df2797
muhamadghibran <ripkaka91@gmail.com>
2024-02-22 23:51:19 +0700

    first

---
+++ MenghitungGaji.py
@@ -0,0 +1,9 @@
+Gajipokok = float(input(" Masukan Gaji Pokok  : Rp. ")) 
+Uangtransport = float(input(" Masukan Uang Transport Harian   : Rp. "))
+Uangmakan = float(input(" Masukan Uang Makan Harian  :Rp.  "))
+Tunjangan = float(input(" Masukan Tunjangan Jabatan :Rp.  "))
+Honorlembur = float(input(" Masukan Honor Lembur :Rp.  "))
+
+TotalGaji = Gajipokok - Uangtransport - Uangmakan + Tunjangan + Honorlembur
+
+print(" Gaji Karyawan Rp : %.0f"%TotalGaji)
